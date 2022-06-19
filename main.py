fdc75cb1016367864b69dc80be69ef40bfd0033f
Daniel Chen <dc17088@thameshigh.school.nz>
2022-06-19 14:29:06 +1200

    Merging Code Into One To make versioning Easier from here on

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 from sys import exit
-from player import Player
 
-
+'''
+Setting Up Main
+'''
 class Game:
     def __init__(self):
         # Player setup
@@ -13,6 +14,48 @@ class Game:
         self.player.update()
         self.player.draw(screen)
         self.player.draw(screen)
+
+'''
+Setting Up Player
+'''
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, constraint, speed):
+        super().__init__()
+        self.image = pygame.image.load("images/player.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect(midbottom=pos)
+        self.speed = speed
+
+        self.max_x_constraint = constraint
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        elif keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+
+    def constraint(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= self.max_x_constraint:
+            self.rect.right = self.max_x_constraint
+
+    def update(self):
+        self.get_input()
+        self.constraint()
+
+'''
+Setting Up First Rubbish Obstacle
+'''
+class Rubbish(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, ):
+        super().__init__()
+        self.image = pygame.image.load("images/Can.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 
 if __name__ == '__main__':
